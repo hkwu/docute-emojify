3ca50666a41fc62b167ddf3ff7d2ae62c21b3642
Kelvin Wu <kelvinwu1996@gmail.com>
2017-03-10 22:06:51 -0500

    Ignore matches found in code.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ function escapeRegExp(string) {
 }
 
 const codes = Object.keys(emoji).map(code => escapeRegExp(code)).join('|');
-const EMOJI_REGEX = new RegExp(`:(${codes}):`, 'g');
+const EMOJI_REGEX = new RegExp(`(\`+)[\\s\\S]+?\\1|:(${codes}):`, 'g');
 
 export default function docuteEmojify() {
-  const replacer = (match, emojiCode) => (emoji[emojiCode] || match);
+  const replacer = (match, ticks, emojiCode) => (emoji[emojiCode] || match);
 
   return ({ beforeParse }) => {
     beforeParse(markdown => markdown.replace(EMOJI_REGEX, replacer));
