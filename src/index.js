969a0b728fdb15daf05daa8a4ee5feaa07515abb
Kelvin Wu <kelvinwu1996@gmail.com>
2017-03-10 22:16:12 -0500

    Document.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import emoji from './emoji';
 
+/**
+ * Escapes RegEx special characters.
+ * @param {string} string - The string to escape.
+ * @returns {string} The escaped string.
+ * @private
+ */
 function escapeRegExp(string) {
   return string.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&');
 }
@@ -7,6 +13,10 @@ function escapeRegExp(string) {
 const codes = Object.keys(emoji).map(code => escapeRegExp(code)).join('|');
 const EMOJI_REGEX = new RegExp(`(\`+)[\\s\\S]+?\\1|:(${codes}):`, 'g');
 
+/**
+ * Plugin which replaces emoji colon forms with their equivalent unicode character.
+ * @returns {Function} The Docute plugin.
+ */
 export default function docuteEmojify() {
   const replacer = (match, ticks, emojiCode) => (emoji[emojiCode] || match);
 
